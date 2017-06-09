968dd2de8e2e23b41c4d4982534153b8b53bfe9c
CHaBou <chabup@delean.fr>
2017-06-09 15:36:42 +0200

    fix(dock): Bounce and badge

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ if (isAlreadyRunning) {
 
 function updateBadge(title) {
 	// ignore `Sindre messaged you` blinking
-	if (title.indexOf('Messenger') === -1) {
+	if (title.indexOf('Work Chat') === -1) {
 		return;
 	}
 
